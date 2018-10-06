ab2334efaa0c0170ec1177fba79927d1f6a4b779
ieexmml <ieexmml@gmail.com>
2018-10-06 12:33:10 +0200

    Notes from class Floating point precison

--- my_cp_framework.cpp
+++ my_cp_framework.cpp
@@ -971,8 +971,35 @@ void TEST_BST(){
     bst.log();
 }
 
+double precise_power(double num, int power){
+    double res = num;
+    for(int i = 1; i < power; i++)
+        res *= num;
+    return res;
+}
+
+void irfan_klub_001(){
+  /*  int n;
+    std::cin >> n;
+    double last = log(102./101.);
+    double test = log(102.0) - log(101.0);
+    std::cout << std::fixed << std::setprecision(20 ) << test << " " << last << std::endl;
+    for(int i = 1; i <= n; i++){
+        last = ((double)(1 << i) - 1.0)/((double)i) - (100.0*last) ;
+    }
+    std::cout << last << std::endl;
+    */
+    double sum = 0;
+    int n = 10;
+    for(int  i = 0; i < 5; i++){
+        sum += (1.0 - (double)(1 << (n+1+i)))/(precise_power(-100.0, i+1)*(n+1.0+i));
+    }
+    std::cout << sum << std::endl;
+}
+
 //}
 
 int main(){
+    irfan_klub_001();
     return 0;
 }
