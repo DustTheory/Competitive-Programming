64f196fadff4011b5ce3f17f65ded1884c297ae3
ieexmml <ieexmml@gmail.com>
2018-10-16 17:53:49 +0200

    Solved UVa 10800 Got AC

--- Problems/UVa_10800.cpp
+++ Problems/UVa_10800.cpp
@@ -2,8 +2,61 @@
 #include <string>
 #include <vector>
 #include <utility>
-
+#include <cmath>
 using namespace std;
+
+vector<string> blank_coordinate_system(int &x, int &y);
+
+void generate_graph(string &s, int &high, int &low){
+    int x = s.length();
+    int y = fabs(high)+fabs(low);
+    std::vector<string> cs = blank_coordinate_system(x, y);
+    std::vector<int> lastchar;
+
+    for(int i = 0; i < y; i++)
+        lastchar.push_back(0);
+    lastchar[y] = x+2;
+
+    int pos = fabs(low);
+
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == 'R'){
+            pos++;
+            cs[y-pos][i+2] = '/';
+            lastchar[y-pos] = i+2;
+        }
+        else if(s[i] == 'F'){
+            cs[y-pos][i+2] = '\\';
+            lastchar[y-pos] = i+2;
+            pos--;
+        }else{
+             cs[y-pos-1][i+2] = '_';
+             lastchar[y-pos-1] = i+2;
+        }
+    }
+
+    for(int i = 0; i < cs.size(); i++){
+        for(int j = 0; j < cs[i].size() && j <= lastchar[i]; j++){
+            cout << cs[i][j];
+        }        
+        cout << endl;
+    }
+}
+
+vector<string> blank_coordinate_system(int &x, int &y){
+    vector<string> cs;
+    for(int i = 0; i < y; i++){
+        string tmp = "|";
+        for(int i = 0; i <= x; i++)
+            tmp += " ";
+        cs.push_back(tmp);
+    }
+    string tmp = "+-";
+    for(int i = 0; i <= x; i++)
+        tmp += "-";
+    cs.push_back(tmp);
+    return cs;
+}
 
 void UVa_10800(){
     int test_cases;
@@ -12,59 +65,30 @@ void UVa_10800(){
         string s;
         cin >> s;
         int height = 0;
-        vector<pair<int, char> > output;
-        int max_height = 0;
+        int low = 0, high = 0;
         for(int i = 0; i < s.length(); i++){
             if(s[i] == 'R')
                 height++;
             else if(s[i] == 'F')
                 height--;
-            if(height > max_height)
-                max_height = height;
-            output.push_back(pair<int, char>(height, s[i]));
+            else if(s[i] == 'C' && height == high)
+                high++;
+            if(height > high)
+                high = height;
+            if(height < low)
+                low = height;
         }
-        vector<string> out;
-        string y_axis = "+";
-        for(int i = 0; i < max_height; i++)
-            y_axis += '|';
-        out.push_back(y_axis);
-
-        for(int i = 0; i < output.size(); i++){
-            string tmp = "-";
-            for(int j = 0; j < max_height; j++)
-                tmp += " ";
-            switch(output[i].second){
-                case 'R':
-                    tmp[output[i].first] = '/';
-                    break;
-                case 'F':
-                    tmp[output[i].first+1] = '\\';
-                    break;
-                default:
-                    tmp[output[i].first+1] = '_';
-                    break;
-            }
-          //  cout << tmp << endl;
-            out.push_back(tmp);
-        }
-        string blank = "";
-        for(int k = 0; k < out.size()+1; k++)
-            blank+= " ";
-        vector<string > outtrans(out[0].size(), blank);
-        for (size_t i = 0; i < out.size(); ++i)
-            for (size_t j = 0; j < out[0].size(); ++j)
-                outtrans[j][i] = out[i][j];
         cout << "Case #" << tc+1 << ":" << endl;
-        for(int i = outtrans.size()-1; i >= 0 ; i--){
-            for(int j = 0; j < outtrans[i].size(); j++){
-                cout << outtrans[i][j];
-            }
-            cout << endl;
-        }
-    }
+        generate_graph(s, high, low);
+        cout << endl;
+  }
 }
 
+
+
+
 int main(){
+    ios_base::sync_with_stdio(false);
     UVa_10800();
     return 0;
 }
