6125dbef1dd82e4a6cb96ca671f3eaa10248adde
Ishak Dervisevic <ieexmml@gmail.com>
2018-10-24 17:09:24 +0200

    Solved UVa_11475 Should get AC, UVa shit it's pants so I can't know. I'll resubmit later

--- Problems/UVa_11475.cpp
+++ Problems/UVa_11475.cpp
@@ -1,67 +1,35 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
-string pal(string &s){
-        for(int i = 0; i < s.length(); i++){
-            int start = 0;
-            bool flag = false;
-            for(int j = i; j < (i+s.length())/2; j++){
-                if(j != s.length()-j-1){
-                    if(s[j] == s[s.length()-j-1]){
-                        if(!flag){
-                            flag = true;
-                            start = j;
-                        }
-                    }else{
-                        flag = false;
-                        start = 0;
-                    }
-                }
-            }
-            if(i == s.length()-1 && start == 0){
-             //   int shift = (s[i] == s[])
-               // cout << "no" << endl;
-                return string(s.rbegin()+1, s.rend());
-            }
-            if(flag){
-              //  cout << start << endl;
-                if(start == 0){
-                    return "";
-                }else{
-                    string res = s.substr(0, start+1);
-                    return s.substr(start+1, s.length()-start-1) + string(res.rbegin(), res.rend());
-                }
-            }
+bool is_pal(string &s, int from, int to){
+    if(to-from == 1)
+        return true;
+    if(to-from == 2){
+        if(s[from] == s[from+1])
+            return true;
+        else return false;
+    }
+    for(int i = from; i < (from+to)/2; i++){
+        if(s[i] != s[to-i+from-1] && i != to-i+from-1){
+            return false;
         }
     }
-
-
-string fix(string &s1, string s2){
-    if(s2.length() == 0)
-        return s1;
-    string res = s1;
-    int pt = 0;
-    for(int i = 0; i < s2.length(); i++){
-        if(s2[i] != s1[s1.length()-i-1])
-            break;
-        pt++;
-    }
-    if(pt == s2.length()){
-        int c = 0;
-        for(int i = 1; i < s2.length() ; i++){
-            if(s2[i] == s2[i-1]){
-                c++;
-            }else{
-                break;
-            }
-        }
-        cout << c << ' ' << s2 << endl;
-        return s1 + s2.substr(c+1, s2.length()-c);
-    }
-    res += s2.substr(pt, s2.length()-pt+1);
-    return res;
+    return true;
 }
+
+string pal(string &s){
+    for(int i = 0; i < s.length(); i++){
+        bool is_p = is_pal(s, i, s.length());
+        if(is_p){
+            string tmp = s.substr(0, i);
+            return string(tmp.rbegin(), tmp.rend());
+        }
+    }
+    return string(s.rbegin(), s.rend());
+}
+
 
 int main(){
     string s;
@@ -71,6 +39,6 @@ int main(){
         if(s.length() == 1)
             cout << s << endl;
         else
-            cout << fix(s, pal(s)) << endl;
+            cout << s << pal(s) << endl;
     }
 }
