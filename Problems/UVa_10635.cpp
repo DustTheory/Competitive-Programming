d30616c90a587beaf18f8dd5c9fab09ac62fa41b
Ishak Dervišević <ieexmml@gmail.com>
2018-10-29 08:59:15 +0100

    Improved UVa 10635, still TLE
    Now we preprocess the arrays to include only numbers that are included in both arrays; Then we do lcs on that

--- Problems/UVa_10635.cpp
+++ Problems/UVa_10635.cpp
@@ -4,19 +4,21 @@
 using namespace std;
 
 #define NAN -420694206
-#define nINF -999999
+#define nINF -99999999
 
 vector<vector<int>> mem;
 
 int lcs(int i , int j, vector<int> &a, vector<int> &b){
 	if(i == -1 || j == -1)
 		return 0;
-	if(mem[i][j] != nINF)
+	if(mem[i][j] != NAN)
 		return mem[i][j];		
-	int score = -9999999;
+	int score = nINF;
 	if(a[i] == b[j])
 		score = 1;
-	int  option1 = lcs(i-1, j-1, a, b) + score;
+	int  option1 = lcs(i-1, j-1, a, b);
+	if(option1 != nINF)
+		 option1 += score;
 	int  option2 = lcs(i-1, j, a, b);
 	int  option3 = lcs(i, j-1, a, b);
 	int m = max(option1, max(option2, option3));
@@ -32,23 +34,47 @@ int main(){
 		cin>> n >> p >> q;
 		vector<int> prince;
 		vector<int> queen;
+		bool square[n*n+1];
+		for(int i = 0; i <= n*n+1; i++)
+		    square[i] = false;
 		for(int i = 0; i <= p; i++){
 			int tmp;
 			cin >> tmp;
+			square[tmp] = true;
 			prince.push_back(tmp);
 		}
+		bool newsquare[n*n+1];
+		for(int i = 0; i <= n*n+1; i++)
+		    newsquare[i] = false;
 		for(int i = 0; i <= q; i++){
 			int tmp;
 			cin >> tmp;
-			queen.push_back(tmp);
+			if(square[tmp]){
+		    	queen.push_back(tmp);
+				newsquare[tmp] = true;
+			}
 		}
+		vector<int> newprince;
+		for(int i =0; i < prince.size(); i++){
+			if(newsquare[prince[i]]){
+				newprince.push_back(prince[i]);
+			}
+		}
+		for(int i = 0; i < newprince.size(); i++){
+			cout << newprince[i] << ' ';
+		}
+		cout << endl;
+		for(int i = 0; i < queen.size(); i++){
+			cout << queen[i] << ' ';
+		}
+		cout << endl;
 		mem.clear();
 		for(int i = 0; i <= p; i++){
 			mem.push_back(vector<int>());
 			for(int j = 0; j <= q; j++){
-				mem[i].push_back(nINF);
+				mem[i].push_back(NAN);
 			}
 		}
-		cout << "Case " << t+1<< ": " << lcs(p, q, prince, queen) << endl;
+		cout << "Case " << t+1<< ": " << lcs(newprince.size()-1, queen.size()-1, newprince, queen) << endl;
 	}
 }
