7c5ff2e70feb8baae568ff5bd271c0bbec1c5147
Ishak Dervisevic <ieexmml@gmail.com>
2018-12-19 20:00:26 +0100

    Working on 10042

--- Problems/UVa_10042.cpp
+++ Problems/UVa_10042.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <bitset>
 using namespace std;
+
+#define N 100000
 
 vector<int> prime;
 vector<int> prime_digits;
@@ -16,39 +19,45 @@ int sum_of_digits(int n){
 }
 
 void generate_prime(){
-    int n = 1000000; // up to 100k
-    bool not_prime[n] = {0};
-    for(int i = 2; i <= sqrt(n); i++)
-        for(int j = i*2; j <= n; j+=i)
-            not_prime[j] = true;
-    
+    bitset<N> not_prime;
+    not_prime.reset();
+    for(int i = 2; i <= sqrt(N); i++)
+        for(int j = i*2; j < N; j+=i)
+            not_prime.set(j);
+
     prime.clear();
-    for(int i = 1; i <= n; i++)
-        if(!not_prime[i]){
+    for(int i = 1; i < N; i++)
+        if(!not_prime.test(i)){
             prime.push_back(i);
             prime_digits.push_back(sum_of_digits(i));
         }
-    
+
 }
 
 int factorize(int n){
-    if(n <= 1000000 && prime[n])
+    if(n < prime.size() && prime[n])
         return -1;
     int sum = 0;
     int i = 1;
     int c = n;
-    while(c!=1){
+    int cnt = 0;
+    while(c!=1 && i < prime.size()){
         while(c%prime[i] == 0){
             sum += prime_digits[i];
             c /= prime[i];
+            cnt++;
         }
         i++;
     }
+    if(cnt <= 2)
+        return -1;
     return sum;
 }
 
 int main(){
     generate_prime();
+    cout << factorize(10000426) << endl;
+    cout << sum_of_digits(10000426) << endl;
     int tc;
     cin >> tc;
     for(int t = 0; t < tc; t++){
@@ -58,7 +67,7 @@ int main(){
             n++;
             if(sum_of_digits(n) == factorize(n)){
                 cout << n << endl;
-                break;    
+                break;
             }
         }
     }
