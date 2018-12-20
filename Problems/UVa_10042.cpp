37972a00a79f5695b7d14185a155ee0f44d4a2ad
Ishak Dervisevic <ieexmml@gmail.com>
2018-12-20 18:36:38 +0100

    Solved UVa 10042, AC

--- Problems/UVa_10042.cpp
+++ Problems/UVa_10042.cpp
@@ -18,6 +18,13 @@ int sum_of_digits(int n){
     return sum;
 }
 
+bool is_prime(int n){
+    for(int i = *(prime.end()-1); i <= sqrt(n); i++)
+        if(n%i == 0)
+            return false;
+    return true;
+}
+
 void generate_prime(){
     bitset<N> not_prime;
     not_prime.reset();
@@ -35,8 +42,6 @@ void generate_prime(){
 }
 
 int factorize(int n){
-    if(n < prime.size() && prime[n])
-        return -1;
     int sum = 0;
     int i = 1;
     int c = n;
@@ -49,15 +54,17 @@ int factorize(int n){
         }
         i++;
     }
-    if(cnt <= 2)
+    if(c != n && c!=1 && is_prime(c)){
+        sum += sum_of_digits(c);
+    }else if(cnt == 1)
         return -1;
     return sum;
 }
 
 int main(){
     generate_prime();
-    cout << factorize(10000426) << endl;
-    cout << sum_of_digits(10000426) << endl;
+ //   cout << factorize(4) << endl;
+ //   cout << sum_of_digits(4) << endl;
     int tc;
     cin >> tc;
     for(int t = 0; t < tc; t++){
