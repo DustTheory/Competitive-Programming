85d992ac40c8b40fbcd3955ea3653b0184673b5b
Ishak Dervisevic <ieexmml@gmail.com>
2018-12-25 19:13:25 +0100

    Solved UVa 00498 and 10268

---
+++ Problems/UVa_00498.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    while(1){
+        string line1, line2;
+        if(cin.eof())
+            break;
+        getline(cin, line1);
+        getline(cin, line2);
+        stringstream ss1(line1), ss2(line2);
+
+        vector<int> c,x;
+
+        int n;
+        while(ss1 >> n)
+            c.push_back(n);
+        while(ss2 >> n)
+            x.push_back(n);
+        for(int i = 0; i < x.size(); i++){
+            long long res = 0;
+            long long curr = 1;
+            for(int j = c.size()-1; j >= 0; j--){
+                res += curr*c[j];
+                curr*=x[i];
+            }
+            cout << res << (i == x.size()-1 ? "\n" : " ");
+        }
+    }
+}
