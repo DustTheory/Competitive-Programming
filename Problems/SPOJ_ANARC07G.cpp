95362ccbc5016a2153cc4ab26fa14c104f80f8d6
Ishak Dervišević <ieexmml@gmail.com>
2019-04-15 08:53:47 +0200

    Update SPOJ_ANARC07G.cpp

--- Problems/SPOJ_ANARC07G.cpp
+++ Problems/SPOJ_ANARC07G.cpp
@@ -11,35 +11,78 @@ using namespace std;
 
 int sn, fm;
 
+struct option{
+    int s, f, t;
+    option(){s = f = t= 0;}
+    void update(){
+        t = sn*s + fm*f;
+    }
+    option(int s, int f): s(s), f(f){
+        update();
+    }
+    option(int t): t(t){}
+    void buys(int _s){
+        s += _s;
+        update();
+    }
+
+    void buyf(int _f){
+        f += _f;
+        update();
+    }
+    bool is_valid(){
+        return !(s == -1 && f == -1);
+    }
+};
+
+option min(option &a, option &b){
+    if(a.t < b.t)
+        return a;
+    return b;
+}
+
 struct node{
     string name;
     vector<node*> children;
-    int S, F;
-    node(string name) : name(name), S(INF), F(INF) {
-
-    }
+    option S, F;
+    option mo;
+    node(string name) : name(name), S(), F(), mo(-1, -1) {}
     void add_child(node* child){
         children.push_back(child);
     }
     void log_tree(int level = 0){
         for(int i = 0; i < level; i++)
             cout << "\t";
-        printf("%s %d %d \n", name.c_str(), S, F);
+        printf("%s %d %d | (%d, %d) | (%d, %d)\n", name.c_str(), S.t, F.t, S.s, S.f, F.s, F.f);
         for(int i = 0; i < children.size(); i++)
             children[i]->log_tree(level+1);
     }
 
-    int min_subtree_size(){
-        S = sn;
-        for(int i = 0; i < children.size(); i++)
-            S += children[i]->min_subtree_size();
-        F = fm;
+    bool is_leaf(){
+        return children.size() == 0;
+    }
+
+    option min_subtree_size(){
+        if(is_leaf())
+            return option(1, 0);
+        if(mo.is_valid())
+            return mo;
+        S = option(1, 0);
+        for(int i = 0; i < children.size(); i++){
+            option op = children[i]->min_subtree_size();
+            S.buys(op.s);
+            S.buyf(op.f);
+        }
+        F = option(0, 1);
         for(int i = 0; i < children.size(); i++){
             for(int j = 0; j < children[i]->children.size(); j++){
-                F += children[i]->children[j]->min_subtree_size();
+                option op = children[i]->children[j]->min_subtree_size();
+                F.buys(op.s);
+                F.buyf(op.f);
             }
         }
-        return min(S, F);
+        mo = min(S, F);
+        return mo;
     }
 };
 
@@ -61,31 +104,41 @@ string find_root_name(){
 
 int main(){
     string s;
-    cin >> sn >> fm;
-    cin.ignore();
+    int tc = 0;
+    bool first = true;
+    ios_base::sync_with_stdio(false);
     while(true){
         getline(cin , s);
-        if(s[0] == 'B')
-            break;
         if(cin.eof())
             break;
-        char c_parent[1010];
-        sscanf(s.c_str(), "%s", c_parent);
-
-        string parent = c_parent;
-        string children = s.substr(parent.length());
-        string child;
-        stringstream children_stream(children);
-        node* parent_node = people.insert({parent, new node(parent)}).first->second;
-        while(children_stream >> child){
-            node* child_node = people.insert({child, new node(child)}).first->second;
-            parent_node->add_child(child_node);
+        if(s[0] >= '0' && s[0] <= '9'){
+            if(!first){
+                string root_name = find_root_name();
+                cout << "ROOT: " << root_name << endl;
+                people[root_name]->log_tree();
+                option op = people[root_name]->min_subtree_size();
+                printf("%d. %d %d %d", tc, op.s, op.f, op.t);
+            }else{
+                first = false;
+            }
+            sscanf(s.c_str(), "%d %d", &sn, &fm);
+            if(sn == 0 && fm == 0)
+                break;
+            people.clear();
+            tc++;
+        }else{
+            char c_parent[5010];
+            sscanf(s.c_str(), "%s", c_parent);
+            string parent = c_parent;
+            string children = s.substr(parent.length());
+            string child;
+            stringstream children_stream(children);
+            node* parent_node = people.insert({parent, new node(parent)}).first->second;
+            while(children_stream >> child){
+                node* child_node = people.insert({child, new node(child)}).first->second;
+                parent_node->add_child(child_node);
+            }
         }
     }
-
-    string root_name = find_root_name();
-    cout << "ROOT: " << root_name << endl;
-    cout << people[root_name]->min_subtree_size() << endl;
-    people[root_name]->log_tree();
     return 0;
 }
